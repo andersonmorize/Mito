d99c63331304822d24927cd30c13e17c2bb53969
Anderson Morize <anderson.morize@cactussystems.com.br>
2022-04-03 20:25:30 -0300

    Obtenha a chave secreta django no arquivo .env

--- src/config/settings.py
+++ src/config/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ac2hdtugse#48i8x6^cy7*xec2j5wdtx5_51c)_9e!cvr44*pw'
+SECRET_KEY = os.environ.get('DJANGO_APP_SECRET_KEY')
 
 DEBUG = False
 ALLOWED_HOSTS = [
