490d6b996b9a4c412605771fe141b18e82c58c70
Anderson Morize <anderson.morize@cactussystems.com.br>
2022-04-03 03:30:21 -0300

    aplicativo dockerizado

--- src/config/settings.py
+++ src/config/settings.py
@@ -123,14 +123,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-# STATIC_URL = '/static/'
-# STATICFILES_DIRS = (BASE_DIR / 'templates/static', )
-# STATIC_ROOT = STATIC_ROOT = BASE_DIR / 'staticfiles'
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-# MEDIA_URL = '/media/'
-# MEDIA_ROOT = BASE_DIR / 'templates/static/media'
-
 STATICFILES_DIRS = (BASE_DIR / 'templates/static', )
 
 STATIC_URL = '/static/static/'
